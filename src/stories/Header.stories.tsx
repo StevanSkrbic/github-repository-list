5f4ffb092a71eeeccc33f350d0831f707ce30826
StevanSkrbic <stevan.skrbic@htecgroup.com>
2021-05-08 20:24:04 +0200

    Add storybook and one simple story example for Heder

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,8 @@
+import Component from 'components/Header/Header';
+
+export const Default = () => <Component isDark handleSwitchTheme={() => undefined} />;
+
+export default {
+	title: 'Header',
+	components: Component,
+};
